b803bc190e54c959d05cef52931b67ae1d7096a8
IamMikee <gamingwithmike2020@gmail.com>
2023-03-05 12:03:21 +0700

    Create myRestaurantChances.js

---
+++ myRestaurantChances.js
@@ -0,0 +1,16 @@
+let temp;
+
+function chance(start, mult, perc) {
+    let total = 0
+    temp = start*(perc/100)
+    // console.log(temp)
+    total += temp
+    for(i=1; i<mult; i++) {
+        let inc = temp*(perc/100)
+        total += inc
+        temp = inc
+    }
+    console.log(start+total)
+}
+
+chance(5, 2, 50)
